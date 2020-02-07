d9caf71448fdc3b7c9af8a005bb5db3e3287e2c6
League Student <league@iMac-16.attlocal.net>
2020-02-06 18:30:55 -0800

    BOB
    Robot

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -9,8 +9,9 @@ public class FlamingNinjaStar {
 		int baseSize = 300;		//the size of the black part of the star
 		int flameSize = 200;		//the length of the flaming arms
 		
-		// 1. Make a new robot, and set it's pen down.
-
+		// 1. Robot BOB=new Robot Make a new robot, and set it's pen down.
+		Robot BOB=new Robot();
+		BOB.penDown();
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
 		
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
@@ -18,23 +19,26 @@ public class FlamingNinjaStar {
 		// 12. Set the robot speed to 100
 
 		// 13. Use a for loop to repeat all of the code below 25 times
+for (int i = 0; i < 25; i++) {
+	
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		BOB.turn(45);
 			// 3. Move the robot 64 pixels
-
+		BOB.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			BOB.turn(-40);
+			BOB.setSpeed(100);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			BOB.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			BOB.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			BOB.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			BOB.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			BOB.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
@@ -42,5 +46,5 @@ public class FlamingNinjaStar {
 	}
 
 }
-
+}
 
--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,38 +1,52 @@
 package section2;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot BOB=new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+BOB.setSpeed(100);
 		// 5. Set the pen width to 5
-
+BOB.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for (int i = 0; i < 4; i++) {
+	
+}
 			// 7. Set the pen color to random
-	
+	BOB.setPenColor(Color.red);
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+BOB.turn(90);
+	BOB.penDown();
+	
 	}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		for (int i = 0; i < 4; i++) {
+		BOB.move(200);
+		BOB.turn(90);
 	}
-
+	}
 	public static void main(String[] args) {
 		new FourSquare().go();
+
+		
 	}
+	
+	
+	
+	
+	
 
 }
 
