6cc9a412dfc1021ab37050966f66d6a7bd0d24ae
League Student <league@iMac-16.attlocal.net>
2020-02-13 18:23:27 -0800

    finished jungle

---
+++ src/section3/Greeter.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	System.out.println("Ryan");
+String age=JOptionPane.showInputDialog("how old are you");
+JOptionPane.showMessageDialog(null,"Happy Thursday");
+JOptionPane.showMessageDialog(null, "I am "+age+" years old");
+
+
+
+
+
+}
+}
